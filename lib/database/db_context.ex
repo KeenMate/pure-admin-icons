9658e716a6320ad28bd8b0cb4000587c23c565c3
Ondrej Valenta <ondrej.valenta@keenmate.com>
2026-04-15 12:01:35 +0200

    - fixed materialized design icons rerendering - stats fixed

--- lib/database/db_context.ex
+++ lib/database/db_context.ex
@@ -440,6 +440,42 @@ defmodule Database.DbContext do
 
 
   @doc """
+  Calls database function public.get_stats_overview
+  
+  Returns: {:ok, [%Models.GetStatsOverviewModel{}]} | {:error, any()}
+  
+  """
+  @spec get_stats_overview(keyword()) :: {:ok, [%Models.GetStatsOverviewModel{}]} | {:error, any()}
+  def get_stats_overview(query_opts \\ []) do
+    Logger.debug("Calling database routine", routine_name: "get_stats_overview")
+
+    sql_params_str =
+      [
+
+      ]
+      |> Enum.filter(fn {_name, value} -> value != :eg_value_not_provided end)
+      |> Enum.with_index(1)
+      |> Enum.map(fn {_tuple, index} -> "$#{index}" end)
+      |> Enum.join(", ")
+
+    sql_query_params =
+      [
+
+      ]
+      |> Enum.filter(fn value -> value != :eg_value_not_provided end)
+
+    query(
+      "select * from public.get_stats_overview(#{sql_params_str})",
+      sql_query_params,
+      query_opts
+    )
+    
+    |> Processors.GetStatsOverviewProcessor.parse_result()
+    
+  end
+
+
+  @doc """
   Calls database procedure public.refresh_icon_metrics_cube
   
   Returns: {:ok, any()} | {:error, any()}
---
+++ lib/database/processors/get_stats_overview_processor.ex
@@ -0,0 +1,47 @@
+# This code has been auto-generated
+# Changes to this file will be lost on next generation
+
+defmodule Database.Processors.GetStatsOverviewProcessor do
+  @moduledoc """
+  Processor for parsing results from public.get_stats_overview
+  """
+
+  alias Database.Models.GetStatsOverviewModel
+
+  @doc """
+  Parse the result of the database query into a list of GetStatsOverviewModel structs
+  """
+  @spec parse_result({:ok, %Postgrex.Result{}} | {:error, any()}) :: {:ok, [%GetStatsOverviewModel{}]} | {:error, any()}
+  def parse_result({:ok, %Postgrex.Result{rows: rows}}) do
+    parsed_results = rows |> Enum.map(&parse_result_row/1)
+
+    errors = parsed_results |> Enum.filter(&(elem(&1, 0) == :error))
+
+    if Enum.empty?(errors) do
+      successful_results = parsed_results |> Enum.map(&elem(&1, 1))
+      {:ok, successful_results}
+    else
+      {:error, errors}
+    end
+  end
+
+  def parse_result({:error, error}), do: {:error, error}
+
+  @doc """
+  Parse a single result row into a GetStatsOverviewModel struct
+  """
+  @spec parse_result_row(list()) :: {:ok, %GetStatsOverviewModel{}} | {:error, any()}
+  def parse_result_row([source_code, period_code, copies, downloads, searches]) do
+    {:ok, %GetStatsOverviewModel{
+      source_code: source_code,
+      period_code: period_code,
+      copies: copies,
+      downloads: downloads,
+      searches: searches
+    }}
+  end
+
+  def parse_result_row(row) do
+    {:error, "Unexpected row format: #{inspect(row)}"}
+  end
+end
